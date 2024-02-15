c396f8152d15562d8db1c28ae95da550b84db6fb
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-02-15 11:40:19 +0900

    Update dependencies and add ClerkProvider

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from "react";
+
+const AuthLayout = ({ children }: PropsWithChildren) => {
+  return <div className="h-full flex items-center justify-center">{children}</div>;
+};
+export default AuthLayout;
